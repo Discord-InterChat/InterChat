0bb105e7151cab7836601da2ec69dd00bccc4822
Supreme1707 <aravindakshabalaji@gmail.com>
2021-11-05 17:02:33 +0530

    Finished message, moved MongoDB connection, added utils

--- executes/network/connect.js
+++ executes/network/connect.js
@@ -4,10 +4,17 @@ const logger = require('../../logger');
 module.exports = {
 	async execute(interaction, connectedList) {
 		const findChannel = await connectedList.findOne({ channel_id: interaction.channel.id });
+		// const findServer = await connectedList.findOne({ server_id: interaction.channel.guild.id });
+
 		if (findChannel) {
 			await interaction.reply('This channel is already connected to the chat network.');
 			return;
 		}
+		/* if (findServer) {
+			const connectedChannel = await interaction.guild.channels.fetch(findServer.channel_id);
+			await interaction.reply(`This server is already connected to the chat network in the channel ${connectedChannel}. Please disconnect from there first.`);
+			return;
+		} */
 		const insertChannel = { channel_id: interaction.channel.id, channel_name: interaction.channel.name, server_id: interaction.guild.id, server_name: interaction.guild.name };
 
 		try {
