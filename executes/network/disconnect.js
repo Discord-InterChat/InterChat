811315c19db7e023fa5218e99a985d407911c70f
Supreme1707 <aravindakshabalaji@gmail.com>
2021-12-26 13:13:26 +0530

    Added log info for disconnect

--- executes/network/disconnect.js
+++ executes/network/disconnect.js
@@ -1,9 +1,12 @@
+const logger = require('../../logger');
+
 module.exports = {
 	async execute(interaction, connectedList) {
 		const findChannel = await connectedList.findOne({ channelId: interaction.channel.id });
 		if (findChannel) {
 			await connectedList.deleteOne({ channelId: interaction.channel.id });
 			await interaction.reply('Disconnected from the network.');
+			logger.info(`${interaction.guild.name} (${interaction.guildId}) has joined the network.`);
 		}
 		else {
 			await interaction.reply('You are not connected to the network.');
