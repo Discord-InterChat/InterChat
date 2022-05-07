d1d17f3f5022fff3576a1506cc2536099973ab23
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-05-07 14:26:12 +0530

    Formatted blacklist files

--- scripts/blacklist/list.js
+++ scripts/blacklist/list.js
@@ -1,39 +1,49 @@
 const { MessageEmbed } = require('discord.js');
 module.exports = {
 	async execute(interaction, database) {
-		// eslint-disable-next-line no-unused-vars
-		const subCommand = interaction.options.getSubcommand();
 		const serverOpt = interaction.options.getString('type');
-		// console.log(serverOpt);
-		// console.log(subCommand);
 
-		if (serverOpt == 'server') {
+		if (serverOpt == 'server') displayServers();
+		else if (serverOpt == 'user') displayUsers();
+
+		async function displayServers() {
 			const blacklistedServers = database.collection('blacklistedServers');
 			const searchCursor = await blacklistedServers.find();
 			const result = await searchCursor.toArray();
-			// const serverList = [];
-			const Embed = new MessageEmbed();
+			const Embed = new MessageEmbed()
+				.setColor('#0099ff')
+				.setAuthor({
+					name: 'Blacklisted Servers:',
+					iconURL: interaction.client.user.avatarURL(),
+				});
 			for (let i = 0; i < result.length; i++) {
-				console.log(`Name: ${result[i].serverName}\nID: ${result[i]._id}\nserverID: ${result[i].serverId}\nReason: ${result[i].reason}\n\n`);
-				Embed.addFields([{ name: result[i].serverName, value: `ID: ${result[i].serverId}\nReason: ${result[i].reason}\n\n` }]);
+				Embed.addFields([
+					{
+						name: result[i].serverName,
+						value: `ID: ${result[i].serverId}\nReason: ${result[i].reason}\n\n`,
+					},
+				]);
 			}
-			Embed.setColor('#0099ff');
-			Embed.setAuthor({ name: 'Blacklisted Servers:', iconURL: interaction.client.user.avatarURL() });
 			interaction.reply({ embeds: [Embed] });
 		}
-
-		else if (serverOpt == 'user') {
+		async function displayUsers() {
 			const blacklistedServers = database.collection('blacklistedUsers');
 			const searchCursor = await blacklistedServers.find();
 			const result = await searchCursor.toArray();
-			// const userList = [];
-			const Embed = new MessageEmbed();
+			const Embed = new MessageEmbed()
+				.setColor('#0099ff')
+				.setAuthor({
+					name: 'Blacklisted Users:',
+					iconURL: interaction.client.user.avatarURL(),
+				});
 			for (let i = 0; i < result.length; i++) {
-				console.log(`Name: ${result[i].username}\nID: ${result[i]._id}\nUserID: ${result[i].userId}\nReason: ${result[i].reason}\n\n`);
-				Embed.addFields([{ name: result[i].username, value: `ID: ${result[i].userId}\nReason: ${result[i].reason}\n\n` }]);
+				Embed.addFields([
+					{
+						name: result[i].username,
+						value: `ID: ${result[i].userId}\nReason: ${result[i].reason}\n\n`,
+					},
+				]);
 			}
-			Embed.setColor('#0099ff');
-			Embed.setAuthor({ name: 'Blacklisted Users:', iconURL: interaction.client.user.avatarURL() });
 			interaction.reply({ embeds: [Embed] });
 		}
 	},
