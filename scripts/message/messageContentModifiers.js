ecb80c7c5e560da645de981b6a41adb2c20be793
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-05-21 18:10:24 +0530

    Added tenor support (bugged)

--- scripts/message/messageContentModifiers.js
+++ scripts/message/messageContentModifiers.js
@@ -4,5 +4,9 @@ module.exports = {
 			if (message.attachments.size > 1) await message.reply('Due to Discord Embed limitations, only the first attachment will be sent.');
 			embed.setImage(message.attachments.first().url);
 		}
+		// Use tenor api instead since tenor links dont end with .gif [bug]
+		if (message.content.startsWith('https://tenor.com/')) {
+			embed.setImage(message.content);
+		}
 	},
 };
