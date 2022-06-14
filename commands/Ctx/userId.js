6091b7a7c3bc9f5e4ea029b3ae8b9702321dd738
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-06-14 18:30:02 +0530

    Update context menu error messages

--- commands/Ctx/userId.js
+++ commands/Ctx/userId.js
@@ -3,12 +3,12 @@ const { ContextMenuCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new ContextMenuCommandBuilder()
-		.setName('userId')
+		.setName('User ID')
 		.setType(3),
 	async execute(interaction) {
 		const args = await interaction.channel.messages.cache.get(interaction.targetId);
 		if (args.author.id != interaction.client.user.id) return await interaction.reply({ content: 'Invalid usage.', ephemeral: true });
-		if (!args.embeds[0].author.url) return await interaction.reply({ content: 'Invalid usage.', ephemeral: true });
+		if (!args || !args.embeds[0] || !args.embeds[0].author || !args.embeds[0].author.url) return await interaction.reply({ content: 'Invalid usage.', ephemeral: true });
 
 		const msgAuthor = args.embeds[0].author.url.split('/');
 		const userId = msgAuthor[msgAuthor.length - 1];
