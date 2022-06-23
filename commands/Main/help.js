7cfe1a212d75221d800c2a73fbf47d9b7ac4028f
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-06-23 16:26:44 +0530

    New Help Command

--- commands/Main/help.js
+++ commands/Main/help.js
@@ -1,23 +1,67 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const { colors } = require('../../utils');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription('Want help? Here it comes!'),
+		.setDescription('Want help? Here it comes!')
+		.addStringOption(option =>
+			option
+				.setName('command')
+				.setRequired(false)
+				.setDescription('Name of command')
+				.setAutocomplete(true)),
 	async execute(interaction) {
-		const commands = interaction.client.commands;
-		let listOfCommands = '';
-		commands.forEach((value, key) => {
-			listOfCommands += `\`${key}\` - ${value.data.description}\n`;
-		});
-		const embed = new MessageEmbed()
-			.setTitle('Help')
-			.setDescription('Hey all, we have some news for you! Check out the `/updates` command to take a look at the future of ChatBot!')
-			.addFields([{ name: 'List of Commands: ', value: listOfCommands }])
-			.setFooter({ text: 'Requested By: ' + interaction.user.tag, iconURL: interaction.user.avatarURL({ dynamic: true }) })
-			.setAuthor({ name: interaction.client.user.username, iconURL: interaction.client.user.avatarURL() })
-			.setTimestamp();
-		await interaction.reply({ embeds: [embed] });
+		const command_option = interaction.options.getString('command');
+		if (!command_option) {
+			const embed = new MessageEmbed()
+				.setAuthor({ name: interaction.client.user.username + ' Help', iconURL: interaction.client.user.avatarURL() })
+				.setFields(interaction.client.help)
+				.setFooter({ text: 'Requested By: ' + interaction.user.tag, iconURL: interaction.user.avatarURL({ dynamic: true }) })
+				.setColor(colors('chatbot'))
+				.setTimestamp();
+
+			return await interaction.reply({ embeds: [embed] });
+		}
+		const command = interaction.client.commands.get(command_option);
+		const commanddata = command.data.toJSON();
+		const commandOps = commanddata.options;
+
+		let options = '';
+
+		if (!command) return interaction.reply('Unkown command!');
+
+		const command_embed = new MessageEmbed()
+			.setTitle(command.data.name.toUpperCase() + ' Help')
+			.setDescription(command.data.description || command.description || 'No Description')
+			.setFooter({ text: '<> - Required | [] - Optional', iconURL: interaction.client.user.avatarURL })
+			.setColor(colors());
+
+		if (commandOps && commandOps[0]?.type != 1 && commandOps[0]?.type != 2) {
+			commandOps.forEach((value) => value.required ? options += ` <${value.name}>` : options += ` [${value.name}]`);
+			command_embed.addFields([
+				{ name: 'Usage: ', value: `\`/${command.data.name + options}\`` },
+			]);
+		}
+
+		if (commandOps && commandOps[0]?.type === 1) {
+			commandOps.forEach((subcommand) => {
+				const subOptions = subcommand.options.map((optionValue) => {
+					return optionValue.required ? ` <${optionValue.name}>` : ` [${optionValue.name}]`;
+				});
+
+				const data = {
+					name:  `${command.data.name} ${subcommand.name}`,
+					value: `${subcommand.description || 'No Description'}\n**Usage: **\`/${command.data.name} ${subcommand.name}${subOptions.length === 0 ? '' : subOptions.join('')}\``,
+				};
+
+				command_embed.addFields([data]);
+			});
+
+		}
+
+
+		return interaction.reply({ embeds: [command_embed] });
 	},
 };
