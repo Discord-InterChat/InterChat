a1b83ed027386d7107dd533d06587d48d37b83e6
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-07-05 16:24:09 +0530

    feat: Deploy commands by folder & guild

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,14 +8,17 @@ const prompt = require('prompt');
 const { stripIndent } = require('common-tags');
 dotenv.config();
 
+
+const clientID = '798748015435055134';
+const server = '969920027421732874';
+
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-const commands = [];
-
 const deployCommands = async () => {
+	const commands = [];
 	fs.readdirSync('./commands').forEach((dir) => {
 		if (fs.statSync(`./commands/${dir}`).isDirectory()) {
-			if (dir === 'private') { return; }
+			if (dir === 'Private' || dir === 'Staff' || dir === 'Testing') return;
 			const commandFiles = fs.readdirSync(`./commands/${dir}`).filter(file => file.endsWith('.js'));
 			for (const commandFile of commandFiles) {
 				const command = require(`./commands/${dir}/${commandFile}`);
@@ -25,70 +28,102 @@ const deployCommands = async () => {
 	});
 
 	// FIXME: Change the ID main ChatBot ID
-	rest.put(Routes.applicationCommands('798748015435055134'), { body: commands })
+	rest.put(Routes.applicationCommands(clientID), { body: commands })
 		.then(() => {
 			logger.info('Registered all application commands successfully');
 		})
 		.catch(console.error);
 };
 
-const deployPrivateCommands = async () => {
-	const commandFiles = fs.readdirSync('./commands/private').filter(file => file.endsWith('.js'));
-	for (const commandFile of commandFiles) {
-		const command = require(`./commands/private/${commandFile}`);
+
+const deployStaffCommands = async () => {
+	const commands = [];
+	const StaffFiles = fs.readdirSync('./commands/Staff').filter(file => file.endsWith('.js'));
+
+	for (const StaffFile of StaffFiles) {
+		const command = require(`./commands/Staff/${StaffFile}`);
 		commands.push(command.data.toJSON());
 	}
 
 	// FIXME: Change the IDs to main ChatBot ID and ChatBot HQ ID
-	rest.put(Routes.applicationGuildCommands('798748015435055134', '969920027421732874'), { body: commands })
+	rest.put(Routes.applicationGuildCommands(clientID, server), { body: commands })
 		.then(() => logger.info('Registered all application commands for\u001b[35m ChatBot HQ\u001b[0m successfully'))
 		.catch((e) => console.error(e));
 };
 
+
+const deplotCustomCommands = async (args, guild) => {
+	const commands = [];
+
+	if (!args) return console.log('Provide Folder Name!');
+
+	const commandFiles = fs.readdirSync(`./commands/${args}`).filter(file => file.endsWith('.js'));
+
+	for (const commandFile of commandFiles) {
+		const command = require(`./commands/${args}/${commandFile}`);
+		commands.push(command.data.toJSON());
+	}
+
+	// FIXME: Change the IDs to main ChatBot ID and ChatBot HQ ID
+	rest.put(Routes.applicationGuildCommands(clientID, guild || server), { body: commands })
+		.then((cmds) => {
+			cmds.forEach((e) => {
+				console.log(`Registered ${e.name} successfully.`);
+			});
+			logger.info(`Registered all application commands for\u001b[35m ${ guild || 'ChatBot HQ' }\u001b[0m successfully`);
+		})
+		.catch((e) => console.error(e));
+};
+
+
 // create a CLI prompt for deployment
-async function commandLine() {
-	const args = process.argv[2]?.toLowerCase();
-	const help = stripIndent`
+const args = process.argv[2]?.toLowerCase();
+const help = stripIndent`
 	Usage:
-		deploy [--private | -p]
+		deploy [--staff | -s]
+		deploy [--custom | -c] <\u001b[30mFolderName\u001b[0m> [GuildID]
 		deploy [--all | -all | --a | -a]
 		deploy [--help | -help | --h | -h]
 	Options:
 		-h, --help    Show this help message and exit.
 		-a, --all     Deploy both public and private commands.
-		-p, --private Deploy private commands.`;
+		-s, --staff   Deploy private commands.
+		-c, --custom  Deploy Commands to ChatBot HQ, or any other guild. Specify the folder you want to deploy from.`;
 
-	switch (args) {
+switch (args) {
+case '--staff':
+case '-s':
+	deployStaffCommands();
+	break;
+case '--custom':
+case '-c': {
+	const arg2 = process.argv[3];
+	const guild = process.argv[4];
+	deplotCustomCommands(arg2, guild);
+	break;
+}
+case '--all':
+case '-all':
+case '--a':
+case '-a':
+	prompt.start();
+	console.log('Are you sure you want to deploy all commands? This will overwrite all commands and make private commands visible to every server. (y/n)');
+	prompt.get(['y/n'], (err, result) => {
+		result['y/n'] === 'y' ? deployCommands() && deployStaffCommands() : console.log('\n\033[31;1;4mDeployment aborted.\033[0m');
+	});
+	break;
+case '--help':
+case '-help':
+case '--h':
+case '-h':
+	console.log(help);
+	break;
 
-	case '--private':
-	case '-p':
-		await deployPrivateCommands();
-		break;
-	case '--all':
-	case '-all':
-	case '--a':
-	case '-a':
-		prompt.start();
-		console.log('Are you sure you want to deploy all commands? This will overwrite all commands and make private commands visible to every server. (y/n)');
-		prompt.get(['y/n'], (err, result) => {
-			result['y/n'] === 'y' ? deployCommands() && deployPrivateCommands() : console.log('\n\033[31;1;4mDeployment aborted.\033[0m');
-		});
-		break;
-	case '--help':
-	case '-help':
-	case '--h':
-	case '-h':
-		console.log(help);
-		break;
+case undefined:
+	deployCommands();
+	break;
 
-	case undefined:
-		deployCommands();
-		break;
-
-	default:
-		console.log('Invalid argument provided. Please use \u001B[40;5;31mdeploy --help\u001B[0m for more information.');
-	}
+default:
+	console.log('Invalid argument provided. Please use \u001B[40;5;31mdeploy --help\u001B[0m for more information.');
 }
 
-commandLine()
-	.catch(console.error);
