e4dcc57ec2fc6837a5a4902536efa7db696d866d
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-08-12 19:00:17 +0530

    fix(server): Fixed `disconnect` command disconnecting wrong server

--- src/Scripts/server/disconnect.js
+++ src/Scripts/server/disconnect.js
@@ -1,18 +1,13 @@
 const logger = require('../../utils/logger');
 
 exports.execute = async (interaction, database) => {
+	const serverId = interaction.options.getString('serverid');
 	const connectedList = database.collection('connectedList');
-	const guildInDb = await connectedList.findOne({ serverId: interaction.guild.id });
+	const guildInDb = await connectedList.findOne({ serverId });
 
-	if (guildInDb) {
-		await connectedList.deleteOne({ serverId: interaction.guild.id });
-		await interaction.reply(`Disconnected ${guildInDb.serverName} from the network.`);
-		logger.info(
-			`${interaction.guild.name} (${interaction.guildId}) has been force disconnected from the network by ${interaction.user.tag}.`,
-		);
-	}
+	if (!guildInDb) return await interaction.reply('Server is not connected to the network.');
 
-	else {
-		await interaction.reply('Server is not connected to the network.');
-	}
+	await connectedList.deleteOne({ serverId });
+	await interaction.reply(`Disconnected ${guildInDb.serverName} from the network.`);
+	logger.info(`${interaction.guild.name} (${interaction.guildId}) has been force disconnected from the network by ${interaction.user.tag}.`);
 };
