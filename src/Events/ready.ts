125b57d12d99232b30d7ea6aa99b1cbc2801647d
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-10-15 13:00:30 +0530

    refactor: Added better comments to ready event

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -7,28 +7,25 @@ import { constants } from '../Utils/functions/utils';
 export default {
 	name: 'ready',
 	once: true,
-
 	async execute(client: Client) {
-		constants.topgg.postStats({ serverCount: client.guilds.cache.size });
-
 		const db = utils.getDb();
+		const messageData = db?.collection('messageData');
 		const FOUR_HOURS = 60 * 60 * 4000;
 
 		// set a property called "expired" to a document that is older than 4 hours.
 		setInterval(async () => {
 			const older_than_four = new Date(Date.now() - FOUR_HOURS); // 4 hours before now
-			const messageInDb = db?.collection('messageData');
-			await messageInDb?.updateMany({ timestamp: { $lte: older_than_four.getTime() } }, { $set: { expired: true } });
+			await messageData?.updateMany({ timestamp: { $lte: older_than_four.getTime() } }, { $set: { expired: true } });
 
 		}, 60 * 60 * 4500);
 
-		// Delete expired documents every 12 hours
+		// Delete all documents that have the property "expired" set to true.
 		setInterval(async () => {
-			const messageInDb = db?.collection('messageData');
-			await messageInDb?.deleteMany({ expired: true });
+			await messageData?.deleteMany({ expired: true });
 		}, 60 * 60 * 12_000);
 
+		constants.topgg.postStats({ serverCount: client.guilds.cache.size });
 
-		logger.info(`Logged in as ${client.user?.tag}`);
+		logger.info(`Logged in as ${client.user?.tag}!`);
 	},
 };
