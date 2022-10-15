bdead8395f11cbdfe84e218ca804c56a7cc97bd7
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-10-15 18:29:42 +0530

    cleanup script

--- src/Events/messageCreate.ts
+++ src/Events/messageCreate.ts
@@ -1,11 +1,11 @@
 import wordFilter from '../Utils/functions/wordFilter';
-import logger from '../Utils/logger';
 import messageContentModifiers from '../Scripts/message/messageContentModifiers';
 import evalScript from '../Scripts/message/evalScript';
 import messageTypes from '../Scripts/message/messageTypes';
 import { EmbedBuilder, GuildMember, Message, User } from 'discord.js';
 import { getDb, colors } from '../Utils/functions/utils';
 import { connectedListDocument } from '../Utils/typings/types';
+import { InvalidChannelId } from '../Scripts/message/cleanup';
 
 // TODO: edit the embed instead of changing the message content
 // if guild has profanity disabled and has embeds on set the embed to normal desc :DDDDDDDDDDDDD
@@ -57,6 +57,9 @@ export default {
 			const checks = await require('../Scripts/message/checks').execute(message, database);
 			if (checks === false) return;
 
+			const allConnectedChannels = connectedList?.find({});
+
+
 			if (message.reference) {
 				const referredMessage = await message.fetchReference();
 				if (referredMessage.author.id === message.client.user.id
@@ -70,7 +73,6 @@ export default {
 			// check if message contains profanity and censor it if it does
 			if (wordFilter.check(message.content)) message.content = wordFilter.censor(message.content);
 
-			const allConnectedChannels = await connectedList?.find().toArray();
 
 			const embed = new EmbedBuilder()
 				.setTimestamp()
@@ -99,50 +101,14 @@ export default {
 			// FIXME: Add levelling back when ready
 			// require('../Scripts/message/levelling').execute(message);
 
-			const channelAndMessageIds: Promise<Message<boolean> | undefined>[] = [];
-			const channelsToDelete: string[] = [];
+			const channelAndMessageIds: Promise<Message | InvalidChannelId | undefined>[] = [];
 
-			for (const channelObj of allConnectedChannels as connectedListDocument[]) {
-				try {
-					await message.client.channels.fetch(channelObj.channelId);
-				}
-				catch {
-					channelsToDelete.push(channelObj.channelId);
-					logger.warn(`Found deleted channel ${channelObj.channelId} in database.`);
-					continue;
-				}
-
+			allConnectedChannels?.forEach(channelObj => {
 				// sending the messages to the connected channels
-				const msg = messageTypes.execute(message.client, message, channelObj, embed, setup, attachments);
+				const msg = messageTypes.execute(message.client, message, channelObj as connectedListDocument, embed, setup, attachments);
 				// push the entire promise, as we dont want to wait for it inside the loop
 				channelAndMessageIds.push(msg);
-			}
-
-			message.delete().catch(() => {return;});
-
-			// TODO make a cleanup script
-
-			// Deleting all found channels that are non existant
-			connectedList?.deleteMany({ channelId: { $in: channelsToDelete } });
-			setup?.deleteMany({ 'channel.id': { $in: channelsToDelete } });
-
-
-			Promise.allSettled(channelAndMessageIds)
-				.then((data) => {
-					const messageDataObj = data.map((msg) => {
-						if (msg.status === 'fulfilled') return { channelId: msg.value?.channelId, messageId: msg.value?.id };
-					});
-
-					// for editing and deleting messages
-					messageData?.insertOne({
-						channelAndMessageIds: messageDataObj,
-						timestamp: message.createdTimestamp,
-						authorId: message.author.id,
-						serverId: message.guild?.id,
-						expired: false,
-					});
-				})
-				.catch(logger.error);
+			}).then(() => require('../Scripts/message/cleanup').default.execute(message, channelAndMessageIds, messageData, connectedList));
 		}
 		else {
 			return;
