567efa7dd06e0b46aa46819d7c51c618744bfd36
dev-737 <73829355+dev-737@users.noreply.github.com>
2023-01-12 19:53:34 +0530

    fix(deletemsg): fix interaction failure

--- src/Commands/Network/deleteMsg.ts
+++ src/Commands/Network/deleteMsg.ts
@@ -1,6 +1,5 @@
 import { ContextMenuCommandBuilder, ApplicationCommandType, MessageContextMenuCommandInteraction, TextChannel } from 'discord.js';
 import { getDb, checkIfStaff } from '../../Utils/functions/utils';
-import { stripIndents } from 'common-tags';
 import logger from '../../Utils/logger';
 import { networkMessageDelete } from '../../Scripts/networkLogs/networkMsgDelete';
 
@@ -9,39 +8,42 @@ export default {
     .setName('Delete Message')
     .setType(ApplicationCommandType.Message),
   async execute(interaction: MessageContextMenuCommandInteraction) {
+    const db = getDb();
     const target = interaction.targetMessage;
     const staffUser = await checkIfStaff(interaction.user);
-
-    const db = getDb();
     const messageInDb = await db?.messageData.findFirst({
       where: { channelAndMessageIds: { some: { messageId: { equals: target.id } } } },
     });
+    const emojis = interaction.client.emoji.normal;
 
-    if (!messageInDb || (messageInDb?.expired && staffUser === false)) {return interaction.reply({ content: 'This message has expired.', ephemeral: true });}
-
-    if (staffUser || interaction.user.id === messageInDb?.authorId) {
-      messageInDb?.channelAndMessageIds.forEach((element) => {
-        if (!element) return;
-
-        interaction.client.channels
-          .fetch(element.channelId)
-          .then((channel) => {
-            (channel as TextChannel).messages
-              .fetch(element.messageId)
-              .then((message) => message.delete())
-              .catch((e) => logger.error('Delete Message:', e));
-          })
-          .catch(logger.error);
-      });
-
-      interaction.inCachedGuild() ? networkMessageDelete(interaction.member, target) : null;
-      interaction.reply({ content: 'Message deleted.', ephemeral: true });
+    if (!messageInDb || (messageInDb?.expired && staffUser === false)) {
+      return interaction.reply({ content: 'This message has expired.', ephemeral: true });
     }
-    else {
-      interaction.reply({
-        content: stripIndents`${interaction.client.emoji.normal.no} Unable to delete message.\nCommon Reasons: Message not sent by you, Message not sent in network.`,
-        ephemeral: true,
-      });
+
+    // if the user tries to edit someone else' message and they arent staff
+    if (!staffUser && interaction.user.id !== messageInDb.authorId) {
+      return interaction.reply({ content: `${emojis.no} You are not the author of this message.`, ephemeral: true });
     }
+
+    await interaction.reply({ content: `${emojis.yes} Deletion in progess! This may take a couple of seconds.`, ephemeral: true }).catch(() => null);
+
+    messageInDb.channelAndMessageIds.forEach((element) => {
+      if (!element) return;
+
+      interaction.client.channels
+        .fetch(element.channelId)
+        .then((channel) => {
+          (channel as TextChannel).messages
+            .fetch(element.messageId)
+            .then((message) => message.delete())
+            .catch((e) => logger.error('Delete Message:', e));
+        })
+        .catch(logger.error);
+    });
+
+    // log the deleted message for moderation purposes
+    interaction.inCachedGuild()
+      ? networkMessageDelete(interaction.member, target)
+      : null;
   },
 };
