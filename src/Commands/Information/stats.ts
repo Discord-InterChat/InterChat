064b9758f6a12306383d8bd3131c659689392856
dev-737 <73829355+dev-737@users.noreply.github.com>
2023-02-21 11:03:06 +0000

    fix(stats): Reverted embed style to original

--- src/Commands/Information/stats.ts
+++ src/Commands/Information/stats.ts
@@ -1,73 +1,52 @@
-import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
-import { stripIndents } from 'common-tags';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { toHuman, getDb, colors } from '../../Utils/functions/utils';
-import os from 'os';
+import { totalmem } from 'os';
 
 export default {
   data: new SlashCommandBuilder()
     .setName('stats')
     .setDescription('Shows the bot\'s statistics'),
   async execute(interaction: ChatInputCommandInteraction) {
-    const uptime = toHuman(interaction.client?.uptime as number);
-    const osUptime = toHuman(os.uptime() * 1000);
-
-    const connectedList = getDb().connectedList;
+    const { connectedList, messageData } = getDb();
     const allConnected = await connectedList?.findMany({});
-    const count = await connectedList?.count();
+    const connectionCount = allConnected.length;
+    const totalNetworkMessages = await messageData.count();
 
-    const docsLink = 'https://discord-chatbot.gitbook.io/docs';
-    const inviteLink = 'https://discord.com/api/oauth2/authorize?client_id=769921109209907241&permissions=154820537425&scope=bot%20applications.commands';
+    const uptime = toHuman(interaction.client.uptime);
+    const docsLink = 'https://discord-chatbot.gitbook.io';
     const supportServer = 'https://discord.gg/6bhXQynAPs';
 
-    let connectedMembers = 0;
-    for (const guildEntry of allConnected) {
-      let guild;
-      try {
-        guild = await interaction.client.guilds.fetch(String(guildEntry.serverId));
-      }
-      catch {
-        continue;
-      }
-      connectedMembers += guild.memberCount;
-    }
-
     const embed = new EmbedBuilder()
-      .setAuthor({ name: `${interaction.client.user?.username} Statistics`, iconURL: interaction.client.user?.avatarURL() as string })
       .setColor(colors('chatbot'))
-      .setDescription(`[Invite](${inviteLink}) • [Support](${supportServer}) • [Docs](${docsLink})`)
+      .setTitle(`${interaction.client.user.username} Statistics`)
+      .setDescription(`__Networks__: ${connectionCount} • __Network Messges__: ${totalNetworkMessages}`)
+      .setFooter({ text: 'Network Messages reset every 24 hours.' })
       .addFields([
-        {
-          name: 'System',
-          value: stripIndents`\`\`\`elm
-					CPU: ${os.cpus()[0].model}
-					Memory: ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)} MB / ${Math.round(os.totalmem() / 1024 / 1024 / 1024)} GB
-					Uptime: ${osUptime}
-					Discord.JS: ${require('discord.js/package.json').version}
-					Node.JS: ${process.version}
-					\`\`\``,
-        },
-        {
-          name: 'Bot',
-          value: stripIndents`\`\`\`elm
-					Ping: ${interaction.client.ws.ping}ms
-					Uptime: ${uptime}
-					Servers: ${interaction.client.guilds.cache.size}
-					Users: ${interaction.client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)}
-					Commands: ${interaction.client.commands.size}
-					Bot Version: v${interaction.client.version}
-					\`\`\``,
-          inline: true,
-        },
-        {
-          name: 'Chat Network',
-          value: stripIndents`\`\`\`elm
-					Servers: ${count}
-					Members: ${connectedMembers}
-					\`\`\``,
-          inline: true,
-        },
+        { name: 'Uptime', value: uptime, inline: true },
+        { name: 'Ping:', value: `${interaction.client.ws.ping}ms`, inline: true },
+        { name: 'Bot Version:', value: `v${interaction.client.version}`, inline: true },
+        { name: 'Cached Servers:', value: `${interaction.client.guilds.cache.size}`, inline: true },
+        { name: 'Ram Usage:', value: `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)} MB / ${Math.round(totalmem() / 1024 / 1024 / 1024)} GB`, inline: true },
+        { name: 'Commands', value: `${interaction.client.commands.size}`, inline: true },
       ]);
-    await interaction.reply({ embeds: [embed] });
+
+    const linksRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setLabel('Support')
+        .setStyle(ButtonStyle.Link)
+        .setURL(supportServer),
+      new ButtonBuilder()
+        .setLabel('Guide')
+        .setStyle(ButtonStyle.Link)
+        .setURL(docsLink),
+      new ButtonBuilder()
+        .setLabel('Invite')
+        .setStyle(ButtonStyle.Link)
+        .setURL(interaction.client.inviteLink),
+    );
+
+
+    await interaction.reply({ embeds: [embed], components: [linksRow] });
   },
 
 };
