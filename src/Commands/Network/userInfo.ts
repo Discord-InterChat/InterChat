a28fd20e0d49af44b56923cd2656e70eb0f3992b
dev-737 <73829355+dev-737@users.noreply.github.com>
2023-03-12 16:28:40 +0530

    feat(userInfo): Cool new profile banner image

--- src/Commands/Network/userInfo.ts
+++ src/Commands/Network/userInfo.ts
@@ -1,5 +1,6 @@
-import { ApplicationCommandType, ContextMenuCommandBuilder, EmbedBuilder, MessageContextMenuCommandInteraction } from 'discord.js';
+import { ApplicationCommandType, AttachmentBuilder, ContextMenuCommandBuilder, EmbedBuilder, MessageContextMenuCommandInteraction } from 'discord.js';
 import { badgeToEmoji, colors, getDb } from '../../Utils/functions/utils';
+import { profileImage } from 'discord-arts';
 
 export default {
   data: new ContextMenuCommandBuilder()
@@ -8,36 +9,37 @@ export default {
   async execute(interaction: MessageContextMenuCommandInteraction) {
     const db = getDb();
     const target = interaction.targetId;
+    const emoji = interaction.client.emoji.normal;
     const messageInDb = await db.messageData.findFirst({
       where: { channelAndMessageIds: { some: { messageId: target } } },
     });
 
     if (!messageInDb) return interaction.reply({ content: 'This message has expired! Please try another message.', ephemeral: true });
     const user = await interaction.client.users.fetch(messageInDb.authorId, { force: true }).catch(() => null);
-    if (!user) return interaction.reply(`${interaction.client.emoji.normal.no} Something went wrong while trying to get user details.`);
+    if (!user) return interaction.reply(`${emoji.no} Unable to fetch user details.`);
 
     const userBadgesRaw = await db.userBadges.findFirst({ where: { userId: user.id } });
     const createdAt = Math.round(user.createdTimestamp / 1000);
 
     const userBadges = userBadgesRaw ? badgeToEmoji(userBadgesRaw?.badges) || 'No Badges.' : 'No Badges.';
 
+
     const embed = new EmbedBuilder()
       .setTitle(user.tag)
       .setThumbnail(user.avatarURL() ?? user.defaultAvatarURL)
-      .setImage(user.bannerURL({ size: 2048 }) ?? null)
       .setColor(colors('invisible'))
-      .addFields(
-        {
-          name: 'Badges',
-          value: userBadges,
-        },
-        {
-          name: 'General Info',
-          value: `**ID:** ${user.id}\n**Discrim:** ${user.tag}\n**Bot Account:** ${user.bot}\n**Created:** <t:${createdAt}:d> (<t:${createdAt}:R>)`,
-        },
-      )
+      .setImage('attachment://customCard.png') // link to image that will be generated afterwards
+      .setDescription(userBadges)
+      .addFields({
+        name: 'General Info',
+        value: `**Name:** ${user.tag}\n**Bot Account:** ${user.bot}\n**Created:** <t:${createdAt}:D> (<t:${createdAt}:R>)`,
+      })
       .setFooter({ text:`ID: ${user.id}` });
 
-    await interaction.reply({ embeds: [embed], ephemeral: true });
+    await interaction.reply({ content: `${emoji.loading} Generating profile card...`, embeds: [embed], ephemeral: true });
+
+    // generate the profile card
+    const customCard = new AttachmentBuilder(await profileImage(user.id), { name: 'customCard.png' });
+    await interaction.editReply({ content: '', files: [customCard] });
   },
 };
