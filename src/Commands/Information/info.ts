695c44aac47f17380b0f90646e41ef30616013e9
dev-737 <73829355+dev-737@users.noreply.github.com>
2023-03-18 19:34:14 +0530

    fix: New look for info command

--- src/Commands/Information/info.ts
+++ src/Commands/Information/info.ts
@@ -5,7 +5,7 @@ import { colors, getCredits } from '../../Utils/functions/utils';
 export default {
   data: new SlashCommandBuilder()
     .setName('info')
-    .setDescription('This command is used to get information about the bot.'),
+    .setDescription('Learn more about InterChat.'),
   async execute(interaction: ChatInputCommandInteraction) {
     const { normal, icons } = interaction.client.emoji;
 
@@ -18,51 +18,35 @@ export default {
       members.push(member);
     }
 
+    const linksDivider = stripIndent`${normal.blueLine.repeat(11)} **LINKS** ${normal.blueLine.repeat(11)}`;
+    const creditsDivider = stripIndent`${normal.blueLine.repeat(11)} **CREDITS** ${normal.blueLine.repeat(11)}`;
+
+
     const embed = new EmbedBuilder()
       .setColor(colors('chatbot'))
-      .setTitle(`${icons.info} ${interaction.client.user.username} Information`)
+      .setTitle(`${icons.info} About ${interaction.client.user.username}`)
       .setThumbnail(interaction.client.user.avatarURL())
-      .setDescription('A growing discord bot which provides a fun server-to-server chat! Talk to other servers from your own!')
-      .addFields([
-        {
-          name: 'Credits',
-          value: stripIndent`
-					Some emojis used on this bot are from [Icons discord server](https://discord.gg/aPvvhefmt3).
+      .setDescription(`
+      A growing discord bot which provides a fun server-to-server chat! Talk to other servers from your own!
 
-						${normal.chatbot_circle} **Design:** 
-						> \`-\` ${members.at(-2)?.tag} (Avatar)
-						> \`-\` ${members[0].tag} (Avatar)
-            > \`-\` ${members.at(-1)?.tag} (Mascot)
+      ${creditsDivider}
+      ${normal.chatbot_circle} **Design:** 
+      ${normal.dotBlue} ${members.at(-2)?.tag} (Avatar)
+      ${normal.dotBlue} ${members[0].tag} (Avatar)
+      ${normal.dotBlue} ${members.at(-1)?.tag} (Mascot)
 
-						${icons.botdev} **Developers:**
-						> \`-\` ${members[0].tag}
-						> \`-\` ${members[1].tag}
-						> \`-\` ${members[2].tag}
+      ${icons.botdev} **Developers:**
+      ${normal.dotBlue} ${members[0].tag}
+      ${normal.dotBlue} ${members[1].tag}
+      ${normal.dotBlue} ${members[2].tag}
 
-						${icons.staff} **Staff:**
-						> \`-\` ${members.at(-3)?.tag}
-            > \`-\` ${members.at(-2)?.tag}
-					`,
-        },
-        {
-          name: `${icons.link} Resources`,
-          value: stripIndent`
-					[Guide](https://interchat.gitbook.io/guide)
-					[TopGG](https://top.gg/bot/769921109209907241)
-					[App Directory](https://discord.com/application-directory/769921109209907241)
-					`,
-          inline: true,
-        },
-        {
-          name: '\u200B',
-          value: stripIndent`
-					[Support Server](https://discord.gg/6bhXQynAPs)
-					[Privacy Policy](https://interchat.gitbook.io/important/privacy)
-					[Terms of Service](https://interchat.gitbook.io/important/terms)
-					`,
-          inline: true,
-        },
-      ])
+      ${icons.staff} **Staff:**
+      ${normal.dotBlue} ${members.at(-3)?.tag}
+      ${normal.dotBlue} ${members.at(-2)?.tag}
+
+      ${linksDivider}
+      [Guide](https://interchat.gitbook.io) • [Invite](${interaction.client.inviteLink}) • [Support Server](https://discord.gg/6bhXQynAPs) • [Vote](https://top.gg/bot/769921109209907241) • [App Directory](https://discord.com/application-directory/769921109209907241) • [ToS](https://interchat.gitbook.io/important/terms) • [Privacy Policy](https://interchat.gitbook.io/important/privacy)
+      `)
       .setFooter({
         text: `Requested by ${interaction.user.tag}`,
         iconURL: interaction.user.avatarURL() ?? interaction.user.defaultAvatarURL,
