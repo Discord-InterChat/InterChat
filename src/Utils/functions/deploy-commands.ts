48daa03ca58f7fc1e644355408133930f44a61bf
dev-737 <73829355+dev-737@users.noreply.github.com>
2023-03-19 21:42:59 +0530

    refactor: Refactored deploy commands

--- src/Utils/functions/deploy-commands.ts
+++ src/Utils/functions/deploy-commands.ts
@@ -10,70 +10,41 @@ import 'dotenv/config';
 
 const clientID = process.env.CLIENT_ID as string;
 const server = process.argv[3]?.toLowerCase() || constants.guilds.cbhq;
-const IgnoredDirs = ['Developer', 'Staff', 'Levelling']; // FIXME: Remove levelling system from here when it is fully implemented
+const staffCommands = ['Developer', 'Staff'];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN as string);
 
-// create a CLI prompt for deployment
-const args = process.argv[2]?.toLowerCase();
-
-switch (args) {
-  case '--staff':
-  case '-s':
-    deployStaffCommands();
-    break;
-
-  case '--help':
-  case '-help':
-  case '--h':
-  case '-h':
-    logger.info(stripIndent`
-	Deploy Commands -
-		Usage:
-			deploy [--staff | -s] [guildId]
-			deploy [--help | -help | --h | -h] [guildId]
-		Options:
-			-h, --help    Show this help message and exit.
-			-s, --staff   Deploy staff commands.
-			[guildId] - The guild ID to deploy to.`);
-    break;
-
-  case undefined:
-    deployCommands();
-    break;
-
-  default:
-    logger.error('Invalid argument provided. Please use \u001B[40;5;31mdeploy --help\u001B[0m for more information.');
-}
-
-
-async function deployCommands() {
+function deployCommands() {
   const commands: unknown[] = [];
-  fs.readdirSync(join(__dirname, '..', '..', 'Commands')).forEach((dir) => {
-    if (fs.statSync(join(__dirname, '..', '..', 'Commands', dir)).isDirectory()) {
-      if (IgnoredDirs.includes(dir)) return;
-      const commandFiles = fs.readdirSync(join(__dirname, '..', '..', 'Commands', dir)).filter(file => file.endsWith('.js'));
+  const commandsPath = join(__dirname, '..', '..', 'Commands');
+
+  fs.readdirSync(commandsPath).forEach((dir) => {
+    if (!staffCommands.includes(dir) && fs.statSync(`${commandsPath}/${dir}`).isDirectory()) {
+      const commandFiles = fs.readdirSync(`${commandsPath}/${dir}`).filter(file => file.endsWith('.js'));
+
       for (const commandFile of commandFiles) {
-        const command = require(join(__dirname, '..', '..', 'Commands', dir, commandFile));
+        const command = require(`../../Commands/${dir}/${commandFile}`);
         commands.push(command.default.data.toJSON());
       }
     }
   });
 
   rest.put(Routes.applicationCommands(clientID), { body: commands })
-    .then(() => logger.info('Registered all application commands successfully'))
+    .then(() => logger.info('Registered all application commands successfully.'))
     .catch(logger.error);
 }
 
 
-async function deployStaffCommands() {
-  const commands: any[] = [];
-  fs.readdirSync(join(__dirname, '..', '..', 'Commands')).forEach((dir) => {
-    if (!IgnoredDirs.includes(dir)) return;
-    if (fs.statSync(join(__dirname, '..', '..', 'Commands', dir)).isDirectory()) {
-      const commandFiles = fs.readdirSync(join(__dirname, '..', '..', 'Commands', dir)).filter(file => file.endsWith('.js'));
+function deployStaffCommands() {
+  const commands: unknown[] = [];
+  const commandsPath = join(__dirname, '..', '..', 'Commands');
+
+  fs.readdirSync(commandsPath).forEach((dir) => {
+    if (staffCommands.includes(dir) && fs.statSync(`${commandsPath}/${dir}`).isDirectory()) {
+      const commandFiles = fs.readdirSync(`${commandsPath}/${dir}`).filter(file => file.endsWith('.js'));
+
       for (const commandFile of commandFiles) {
-        const command = require(join(__dirname, '..', '..', 'Commands', dir, commandFile));
+        const command = require(`../../Commands/${dir}/${commandFile}`);
         commands.push(command.default.data.toJSON());
       }
     }
@@ -82,9 +53,47 @@ async function deployStaffCommands() {
   rest.put(Routes.applicationGuildCommands(clientID, server), { body: commands })
     .then(() => {
       rest.get(Routes.guild(server))
-        .then((res: any) => {
-          return logger.info(`Registered Staff application commands for \u001b[35m${res.name}\u001b[0m successfully`);
-        });
-    })
-    .catch(logger.error);
+        .then((res: any) => logger.info(`Registered Staff application commands for \u001b[35m${res.name}\u001b[0m successfully.`));
+    }).catch(logger.error);
+}
+
+// parse command line args to determine which type of deploy
+const args = process.argv[2]?.toLowerCase();
+
+switch (args) {
+  case '--staff':
+  case '-s':
+    deployStaffCommands();
+    break;
+
+  case '-b':
+  case '--both':
+    deployCommands();
+    deployStaffCommands();
+    break;
+
+  case undefined:
+    deployCommands();
+    break;
+
+  case '--help':
+  case '-help':
+  case '--h':
+  case '-h':
+    logger.info(stripIndent`
+    Deploy Application Commands -
+      Usage:
+        deploy [--staff | -s] [guildId]
+        deploy [--both | -b] [guildId]
+        deploy [--help | -help | --h | -h]
+      Options:
+        -h, --help    Show this help message and exit.
+        -s, --staff   Deploy staff commands.
+                          -b --both     Deploy both staff and normal commands.
+        [guildId] - The guild ID to deploy to.`);
+    break;
+
+  default:
+    logger.error('Invalid argument provided. Please use \u001B[40;5;31mdeploy --help\u001B[0m for more information.');
+    break;
 }
