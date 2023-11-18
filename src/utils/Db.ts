4f05938849219855a9fa1cc5d09a96997b951a04
dev-737 <73829355+dev-737@users.noreply.github.com>
2023-11-18 18:13:44 +0530

    fix(network): add caching for fetching connections

--- src/utils/Db.ts
+++ src/utils/Db.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const db = new PrismaClient();
+const db = new PrismaClient().$extends(withAccelerate());
 
 export default db;
