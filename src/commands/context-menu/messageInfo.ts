978ded0b4b3f66393cfa61133659352dc82cd6a7
dev-737 <73829355+dev-737@users.noreply.github.com>
2024-01-25 07:36:41 +0530

    fix(msgInfo): fix problem with reporting compact messages

--- src/commands/context-menu/messageInfo.ts
+++ src/commands/context-menu/messageInfo.ts
@@ -350,7 +350,7 @@ export default class MessageInfo extends BaseCommand {
     const reason = interaction.fields.getTextInputValue('reason');
     const message = await interaction.channel?.messages.fetch(messageId).catch(() => null);
     const content = message?.content || message?.embeds[0].description || undefined;
-    const attachmentUrl = content?.match(REGEX.IMAGE_URL)?.at(0) ?? message?.embeds[0].image?.url;
+    const attachmentUrl = content?.match(REGEX.IMAGE_URL)?.at(0) ?? message?.embeds[0]?.image?.url;
 
     await interaction.client.reportLogger.log(messageInDb.originalMsg.hub.id, {
       userId: authorId,
