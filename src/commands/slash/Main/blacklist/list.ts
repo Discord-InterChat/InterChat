660e54d769501855f3f8a5812196b83ce4785e8f
dev-737 <73829355+dev-737@users.noreply.github.com>
2024-05-18 20:53:18 +0530

    refactor: Update `ListBlacklists` command to use type guards and improve code readability

--- src/commands/slash/Main/blacklist/list.ts
+++ src/commands/slash/Main/blacklist/list.ts
@@ -1,11 +1,16 @@
-import { ChatInputCommandInteraction, EmbedBuilder, time } from 'discord.js';
+import { ChatInputCommandInteraction, EmbedBuilder, User, time } from 'discord.js';
 import db from '../../../../utils/Db.js';
 import BlacklistCommand from './index.js';
 import { paginate } from '../../../../utils/Pagination.js';
 import { colors, emojis } from '../../../../utils/Constants.js';
-import { simpleEmbed } from '../../../../utils/Utils.js';
-import { t } from '../../../../utils/Locale.js';
-import { blacklistedServers, userData } from '@prisma/client';
+import { simpleEmbed, toTitleCase } from '../../../../utils/Utils.js';
+import { supportedLocaleCodes, t } from '../../../../utils/Locale.js';
+import { Prisma, blacklistedServers, userData } from '@prisma/client';
+
+// Type guard functions
+function isServerType(list: blacklistedServers | userData): list is blacklistedServers {
+  return 'serverId' in list;
+}
 
 export default class ListBlacklists extends BlacklistCommand {
   async execute(interaction: ChatInputCommandInteraction) {
@@ -36,121 +41,87 @@ export default class ListBlacklists extends BlacklistCommand {
       return;
     }
 
-    let embeds: EmbedBuilder[] = [];
-    const LIMIT = 5;
-
     const blacklistType = interaction.options.getString('type') as 'server' | 'user';
-    const options = { LIMIT, iconUrl: hubInDb.iconUrl };
     const hubId = hubInDb.id;
-
-    if (blacklistType === 'server') {
-      const result = await db.blacklistedServers.findMany({ where: { hubs: { some: { hubId } } } });
-      embeds = await this.buildServersEmbeds(interaction, hubId, result, options);
-    }
-    else if (blacklistType === 'user') {
-      const result = await db.userData.findMany({ where: { blacklistedFrom: { some: { hubId } } } });
-      embeds = await this.buildUserEmbeds(interaction, hubId, result, options);
-    }
+    const list =
+      blacklistType === 'server'
+        ? await db.blacklistedServers.findMany({ where: { hubs: { some: { hubId } } } })
+        : await db.userData.findMany({ where: { blacklistedFrom: { some: { hubId } } } });
+    const options = { LIMIT: 5, iconUrl: hubInDb.iconUrl };
+    const embeds = await this.buildEmbeds(interaction, list, hubId, options);
 
     await paginate(interaction, embeds);
   }
 
-  private async buildServersEmbeds(
+  private async buildEmbeds(
     interaction: ChatInputCommandInteraction,
+    list: blacklistedServers[] | userData[],
     hubId: string,
-    list: blacklistedServers[],
     opts: { LIMIT: number; iconUrl?: string },
-  ) {
-    const { locale } = interaction.user;
+  ): Promise<EmbedBuilder[]> {
     const embeds: EmbedBuilder[] = [];
-    let fields = [];
+    const fields = [];
     let counter = 0;
+    const type = isServerType(list[0]) ? 'server' : 'user';
 
-    for (let i = 0; i < list.length; i++) {
-      const data = list[i];
-      const hubData = data.hubs.find((d) => d.hubId === hubId);
+    for (const data of list) {
+      const hubData = isServerType(data)
+        ? data.hubs.find((d) => d.hubId === hubId)
+        : data.blacklistedFrom.find((d) => d.hubId === hubId);
+
       const moderator = hubData?.moderatorId
-        ? await interaction.client.users.fetch(hubData?.moderatorId).catch(() => null)
+        ? await interaction.client.users.fetch(hubData.moderatorId).catch(() => null)
         : null;
 
-      fields.push({
-        name: data.serverName,
-        value: t(
-          { phrase: 'blacklist.list.server', locale },
-          {
-            serverId: data.serverId,
-            moderator: moderator ? `@${moderator.username} (${hubData?.moderatorId})` : 'Unknown',
-            reason: `${hubData?.reason}`,
-            expires: !hubData?.expires
-              ? 'Never.'
-              : `${time(Math.round(hubData.expires.getTime() / 1000), 'R')}`,
-          },
-        ),
-      });
+      fields.push(
+        this.createFieldData(data, type, { hubData, moderator, locale: interaction.user.locale }),
+      );
 
       counter++;
-      if (counter >= opts.LIMIT || i === list.length - 1) {
+      if (counter >= opts.LIMIT || fields.length === list.length) {
         embeds.push(
-          new EmbedBuilder().setFields(fields).setColor('#0099ff').setAuthor({
-            name: 'Blacklisted Servers:',
-            iconURL: opts.iconUrl,
-          }),
+          new EmbedBuilder()
+            .setFields(fields)
+            .setColor(colors.invisible)
+            .setAuthor({
+              name: `Blacklisted ${toTitleCase(type)}s:`,
+              iconURL: opts.iconUrl,
+            }),
         );
 
         counter = 0;
-        fields = [];
+        fields.length = 0; // Clear fields array
       }
     }
 
     return embeds;
   }
-
-  private async buildUserEmbeds(
-    interaction: ChatInputCommandInteraction,
-    hubId: string,
-    list: userData[],
-    opts: { LIMIT: number; iconUrl?: string },
+  private createFieldData(
+    data: blacklistedServers | userData,
+    type: string,
+    {
+      moderator,
+      locale,
+      hubData,
+    }: {
+      moderator: User | null;
+      locale?: supportedLocaleCodes;
+      hubData?: Prisma.$hubBlacklistPayload['scalars'];
+    },
   ) {
-    const { locale } = interaction.user;
-    const embeds: EmbedBuilder[] = [];
-    let fields = [];
-    let counter = 0;
-
-    for (let i = 0; i < list.length; i++) {
-      const data = list[i];
-      const hubData = data.blacklistedFrom.find((d) => d.hubId === hubId);
-      const moderator = hubData?.moderatorId
-        ? await interaction.client.users.fetch(hubData?.moderatorId).catch(() => null)
-        : null;
-
-      fields.push({
-        name: `${data.username}`,
-        value: t(
-          { phrase: 'blacklist.list.user', locale },
-          {
-            userId: data.userId,
-            moderator: moderator ? `@${moderator.username} (${hubData?.moderatorId})` : 'Unknown',
-            reason: `${hubData?.reason}`,
-            expires: !hubData?.expires
-              ? 'Never.'
-              : `${time(Math.round(hubData.expires.getTime() / 1000), 'R')}`,
-          },
-        ),
-      });
-
-      counter++;
-      if (counter >= opts.LIMIT || i === list.length - 1) {
-        embeds.push(
-          new EmbedBuilder().setFields(fields).setColor(colors.invisible).setAuthor({
-            name: 'Blacklisted Users:',
-            iconURL: opts.iconUrl,
-          }),
-        );
-
-        counter = 0;
-        fields = [];
-      }
-    }
-    return embeds;
+    return {
+      name: isServerType(data) ? data.serverName : data.username ?? 'Unknown User.',
+      value: t(
+        { phrase: `blacklist.list.${type}`, locale },
+        {
+          id: isServerType(data) ? data.serverId : data.userId,
+          moderator: moderator ? `@${moderator.username} (${moderator.id})` : 'Unknown',
+          reason: `${hubData?.reason}`,
+          expires: !hubData?.expires
+            ? 'Never.'
+            : `${time(Math.round(hubData?.expires.getTime() / 1000), 'R')}`,
+        },
+      ),
+    };
   }
 }
