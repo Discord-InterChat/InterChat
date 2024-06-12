11d7c2944cd91c184ddcb52c5d8bc0a384ec2cf3
dev-737 <73829355+dev-737@users.noreply.github.com>
2024-06-12 15:18:39 +0530

    fix: sync cache immediately when deleting multiple connections

--- src/utils/ConnectedList.ts
+++ src/utils/ConnectedList.ts
@@ -3,16 +3,19 @@ import Logger from './Logger.js';
 import { Prisma, connectedList } from '@prisma/client';
 import { Collection } from 'discord.js';
 import { handleError } from './Utils.js';
+import { captureException } from '@sentry/node';
 
 /** 📡 Contains all the **connected** channels from all hubs. */
 export const connectionCache = new Collection<string, connectedList>();
 export const messageTimestamps = new Collection<string, Date>();
 
-export const syncConnectionCache = async () => {
+export const syncConnectionCache = async (
+  where: Prisma.connectedListWhereInput = { connected: true },
+) => {
   const start = performance.now();
   Logger.debug('[InterChat]: Started populating connection cache.');
 
-  const connections = await db.connectedList.findMany({ where: { connected: true } });
+  const connections = await db.connectedList.findMany({ where });
 
   if (connectionCache.size > 0) {
     connectionCache.forEach((c) => {
@@ -38,10 +41,7 @@ export const deleteConnection = async (where: Prisma.connectedListWhereUniqueInp
 
 export const deleteConnections = async (where: Prisma.connectedListWhereInput) => {
   await db.connectedList.deleteMany({ where });
-
-  syncConnectionCache()
-    .then(() => null)
-    .catch(() => null);
+  await syncConnectionCache().catch(captureException);
 };
 
 export const connectChannel = async (data: Prisma.connectedListCreateInput) => {
@@ -55,8 +55,9 @@ export const modifyConnection = async (
   where: Prisma.connectedListWhereUniqueInput,
   data: Prisma.connectedListUpdateInput,
 ) => {
-  const connection = await db.connectedList.update({ where, data });
-  connectionCache.set(connection.channelId, connection);
+  const connection = await db.connectedList.update({ where, data }).catch(() => null);
+  if (connection) connectionCache.set(connection.channelId, connection);
+
   return connection;
 };
 
@@ -64,13 +65,8 @@ export const modifyConnections = async (
   where: Prisma.connectedListWhereInput,
   data: Prisma.connectedListUpdateInput,
 ) => {
-  const connections = await db.connectedList.updateMany({ where, data });
-
-  syncConnectionCache()
-    .then(() => null)
-    .catch(() => null);
-
-  return connections;
+  await db.connectedList.updateMany({ where, data });
+  await syncConnectionCache(where).catch(captureException);
 };
 
 export const storeMsgTimestamps = (data: Collection<string, Date>): void => {
