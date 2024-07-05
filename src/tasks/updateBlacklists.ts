a53b3d36d62832824b47e610b748a2f9a3f14aeb
dev-737 <73829355+dev-737@users.noreply.github.com>
2024-07-05 15:51:14 +0530

    fix(blacklist): expire blacklists for servers too

--- src/tasks/updateBlacklists.ts
+++ src/tasks/updateBlacklists.ts
@@ -1,3 +1,4 @@
+import { blacklistedServers, userData } from '@prisma/client';
 import { ClusterManager } from 'discord-hybrid-sharding';
 import { Client } from 'discord.js';
 
@@ -6,25 +7,30 @@ export default async (manager: ClusterManager) => {
     async (_client) => {
       const client = _client as unknown as Client;
       const allUsers = await client.userManager.getAllBlacklists();
+      const allServers = await client.serverBlacklists.getAllBlacklists();
 
-      // TODO: same for serverBlacklists
-      allUsers?.forEach((user) => {
-        user?.blacklistedFrom.forEach(async (bl) => {
-          if (bl.expires && new Date(String(bl.expires)) <= new Date()) {
-            if (client.user) {
-              await client.userManager.logUnblacklist(bl.hubId, user.id, {
-                mod: client.user,
-                reason: 'Blacklist expired for user.',
-              });
+      const unblacklistUsers = (entities: (userData | blacklistedServers)[] | null) => {
+        entities?.forEach((entity) => {
+          entity?.blacklistedFrom.forEach(async (bl) => {
+            if (bl.expires && new Date(String(bl.expires)) <= new Date()) {
+              if (client.user) {
+                await client.userManager.logUnblacklist(bl.hubId, entity.id, {
+                  mod: client.user,
+                  reason: 'Blacklist expired for user.',
+                });
+              }
+              const upd = await client.userManager.removeBlacklist(bl.hubId, entity.id);
+
+              console.log(
+                `Updated blacklist for entity ${entity.id}. Total entities: ${entities?.length}. Updated: ${upd}`,
+              );
             }
-            const upd = await client.userManager.removeBlacklist(bl.hubId, user.id);
-
-            console.log(
-              `Updated blacklist for user ${user.id}. Total users: ${allUsers?.length}. Updated: ${upd}`,
-            );
-          }
+          });
         });
-      });
+      };
+
+      unblacklistUsers(allServers);
+      unblacklistUsers(allUsers);
     },
     { shard: 0 },
   );
