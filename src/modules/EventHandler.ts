39631da65722fa584c317a969bbb029f79291f89
dev-737 <73829355+dev-737@users.noreply.github.com>
2024-09-21 09:42:42 +0000

    refactor: use asynchronous fs

--- src/modules/EventHandler.ts
+++ src/modules/EventHandler.ts
@@ -1,7 +1,7 @@
-import Factory from '#main/core/Factory.js';
 import BaseEventListener from '#main/core/BaseEventListener.js';
+import Factory from '#main/core/Factory.js';
 import { ClientEvents, Collection } from 'discord.js';
-import { readdirSync } from 'fs';
+import { readdir } from 'fs/promises';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -12,11 +12,13 @@ export default class EventHandler extends Factory {
   private listeners: Map<string, BaseEventListener<keyof ClientEvents>> = new Collection();
 
   /** Loads all event listeners from the 'events' directory. */
-  public loadListeners(): void {
+  public async loadListeners(): Promise<void> {
     const listenersPath = join(__dirname, '..', 'events');
-    const files = readdirSync(listenersPath).filter((file) => file.endsWith('.js'));
+    const files = await readdir(listenersPath);
 
     files.forEach(async (file) => {
+      if (!file.endsWith('.js')) return;
+
       const { default: Listener } = await import(importPrefix + join(listenersPath, file));
       const listenerInstance: BaseEventListener<keyof ClientEvents> = new Listener();
       this.registerListener(listenerInstance);
