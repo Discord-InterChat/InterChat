9ef89865a4f3dfb4411c7d1ff63e2237d090bc56
dev-737 <73829355+dev-737@users.noreply.github.com>
2025-02-03 11:49:00 +0530

    add licence header

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025 InterChat
+ *
+ * InterChat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * InterChat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with InterChat.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import type { Guild } from 'discord.js';
 import BaseEventListener from '#src/core/BaseEventListener.js';
 import { deleteConnections } from '#utils/ConnectedListUtils.js';
--- src/modules/Loaders/EventLoader.ts
+++ src/modules/Loaders/EventLoader.ts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025 InterChat
+ *
+ * InterChat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * InterChat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with InterChat.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { type Client, type ClientEvents, Collection } from 'discord.js';
